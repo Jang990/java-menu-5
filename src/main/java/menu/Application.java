4b88e64ac120cb96e1c46de1653a89fe7885637a
Jang_Laptop <sdsd090811@gmail.com>
2024-12-13 14:59:16 +0900

    feat: main 메소드에 코치 데이터 Reader 생성

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,32 @@
 package menu;
 
+import menu.io.InputReader;
+import menu.io.coach.CoachDataReader;
+import menu.io.coach.CoachNameReader;
+import menu.io.coach.NonPreferredFoodReader;
+import menu.io.retry.CoachDataReaderErrorLoggingAspect;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CoachDataReader coachDataReader = coachDataReader();
+    }
+
+    private static CoachDataReader coachDataReader() {
+        return new CoachDataReaderErrorLoggingAspect(
+                new CoachDataReader(
+                        coachNameReader(inputReader()),
+                        nonPreferredFoodReader(inputReader())));
+    }
+
+    private static InputReader inputReader() {
+        return new InputReader();
+    }
+
+    private static CoachNameReader coachNameReader(InputReader inputReader) {
+        return new CoachNameReader(inputReader);
+    }
+
+    private static NonPreferredFoodReader nonPreferredFoodReader(InputReader inputReader) {
+        return new NonPreferredFoodReader(inputReader);
     }
 }
